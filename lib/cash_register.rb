e1ecddfe20e528835be60d296330a68e71f85974
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-24 22:51:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+class CashRegister
+
+  def initialize(employee_discount = nil)
+    @total = 0
+    @discount = employee_discount 
+    @purchased_stuff = []
+  end
+  
+  def total= (input_total)
+    @total = input_total
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(title, price, qty = 1)
+    total_price_for_item = price * qty
+    @total = @total + total_price_for_item
+  end
+  
+  def apply_discount
+    
+    
+  end
+end
 
