fc273480bf20c859b3beaa93b9a978c408ab15ba
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-24 22:52:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,8 +20,31 @@ class CashRegister
   end
   
   def apply_discount
-    
-    
+    if @discount != nil
+      step_one = 100 - @discount
+      adjusted_percentage = step_one / 100.00
+      step_two = @total * adjusted_percentage
+      if step_two.class == Float
+        final_total = step_two.to_i
+      elsif step_two.class == Fixnum
+        final_total = step_two
+      end
+      @total = final_total
+      "After the discount, the total comes to $#{final_total}."
+    else
+      "There is no discount to apply."
+    end
   end
+  def items
+    @purchased_stuff
+  end
+  def void_last_transaction
+    complete_cost = @last_transaction[1] * @last_transaction[2]
+    @total = @total - complete_cost
+    @last_transaction[2].times do
+      @purchased_stuff.delete_at(@purchased_stuff.index(@last_transaction[0]))
+    end
+  end
+end
 end
 
